6f69dddca9cb4a2422a55089b93b3da8209ff895
Ky Pham <phamkyptit@gmail.com>
2018-12-03 10:37:35 +0700

    update

--- wp-content/themes/hktravel/search.php
+++ wp-content/themes/hktravel/search.php
@@ -5,8 +5,10 @@
 			<div class="tab-content">
 				<div role="tabpanel" class="tab-pane active" id="home1">
 					<div class="detail-post-main">
-						<ul>       
-							<h2>Kết Quả Tìm</h2>
+						<ul>
+						<div class="panel panel-default">       
+							<h2 style=>Kết Quả Tìm</h2>
+						</div>
 							<?php if(have_posts()) : while(have_posts()) : the_post() ?>
 								<li>
 									<div class="time-line">
