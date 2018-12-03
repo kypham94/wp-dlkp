d6e26d44784b4f42ff76d8811de7acfb0e45d009
Ky Pham <phamkyptit@gmail.com>
2018-12-03 10:12:50 +0700

    update

--- wp-content/themes/hktravel/search.php
+++ wp-content/themes/hktravel/search.php
@@ -3,12 +3,15 @@
     <div id="searchResults">       
         <h2>Kết Quả Tìm</h2>
         <?php if(have_posts()) : while(have_posts()) : the_post() ?>
-        <div class="search">
-            <h1><?php the_title();?></h1>
-                <img src="<?php bloginfo('stylesheet_directory'); ?>/timthumb.php?src=<?php echo get_post_meta($post->ID,'Thumb',true)?>&a=t&w=130&h=130" border="0" />
-                <?php the_content_rss('', false,'', 40);?>
-            </p>
-        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+								<div class="detail-post">
+									<span class="line"></span>
+									<a href="<?php the_permalink(); ?>">
+										<?php echo get_the_post_thumbnail( get_the_id(), 'post-thumb', array("alt"=>get_the_title(), 'class' => 'img-post') ); ?>
+									</a>
+									<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+								</div>
+							</div>
             <?php endwhile; else: ?>
             <p>Tìm không ra! :(</p>
             <?php endif; ?>
