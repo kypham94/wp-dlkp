3d6c854d4da2682e4a829d32e47579431ad00d2e
kypham94 <41797409+kypham94@users.noreply.github.com>
2018-12-11 18:58:14 +0700

    Update index.php

--- wp-content/themes/hktravel/index.php
+++ wp-content/themes/hktravel/index.php
@@ -6,31 +6,31 @@
 				<h2><span>Bài viết nổi bật</span></h2>
 				<div class="main-featured">
 					<div class="row">
-						<div class="col-xs-6 col-sm-4 col-md-4" style="height:500px; background:red">
+						<div class="col-xs-6 col-sm-4 col-md-4" style="height:510px; background:red">
 							
 								<img src="https://dulichkhampha.net/wp-content/uploads/2018/12/1.jpg">
 							
 						</div>
 						
-						<div class="col-xs-6 col-sm-4 col-md-4" style="height:235px; padding-bottom:10px">
+						<div class="col-xs-6 col-sm-4 col-md-4" style="height:235px; padding-bottom:5px">
 							
 								<img src="https://dulichkhampha.net/wp-content/uploads/2018/12/2.jpg">
 							
 						</div>
 						
-						<div class="col-xs-6 col-sm-4 col-md-4" style="height:235px; padding-bottom:10px">
+						<div class="col-xs-6 col-sm-4 col-md-4" style="height:235px; padding-bottom:5px">
 							
 								<img src="https://dulichkhampha.net/wp-content/uploads/2018/12/3.jpg">
 						
 						</div>
 						
-						<div class="col-xs-6 col-sm-4 col-md-4" style="height:235px; padding-bottom:10px">
+						<div class="col-xs-6 col-sm-4 col-md-4" style="height:235px; padding-bottom:5px">
 							
 								<img src="https://dulichkhampha.net/wp-content/uploads/2018/12/4.jpg">
 						
 						</div>
 						
-						<div class="col-xs-6 col-sm-4 col-md-4" style="height:235px; padding-bottom:10px">
+						<div class="col-xs-6 col-sm-4 col-md-4" style="height:235px; padding-bottom:5px">
 						
 								<img src="https://dulichkhampha.net/wp-content/uploads/2018/12/5.jpg">
 							
