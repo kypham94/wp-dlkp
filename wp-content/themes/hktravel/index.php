d448be5e53cf3df8d51d9cc5269d5f7e561aa1b5
kypham94 <41797409+kypham94@users.noreply.github.com>
2018-12-11 23:25:46 +0700

    Update index.php

--- wp-content/themes/hktravel/index.php
+++ wp-content/themes/hktravel/index.php
@@ -9,7 +9,7 @@
 						
 						<div class="col-xs-12 col-sm-12 col-md-4" style="height:515px;">
 							<img src="https://dulichkhampha.net/wp-content/uploads/2018/12/1.jpg" style="height:515px;">
-							<h5 class="heading_title"><a href="https://dulichkhampha.net/gioi-thieu">Giới thiệu</a></h5>
+							<h5 class="heading_title" style="bottom:0px"><a href="https://dulichkhampha.net/gioi-thieu">Giới thiệu</a></h5>
 						</div>
 						
 						<div class="col-xs-12 col-sm-12 col-md-4" style="height:235px; margin-bottom: 35px;">
