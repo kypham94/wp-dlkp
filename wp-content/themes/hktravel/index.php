f56310241b6c731f3eb9015830c4ea50f8e8e465
kypham94 <41797409+kypham94@users.noreply.github.com>
2018-12-12 01:52:55 +0000

    Update index.php

--- wp-content/themes/hktravel/index.php
+++ wp-content/themes/hktravel/index.php
@@ -23,14 +23,31 @@
 								<a href="https://dulichkhampha.net/du-lich"><i class="fa fa-map-marker"></i>Du lịch</a>
 							</h5>
 						</div>
-					</div>
-						
-						<div class="col-xs-12 col-sm-12 col-md-4" style="height:245px; margin-bottom: 35px;">
-							<a href="https://dulichkhampha.net/du-lich">
-								<img src="https://dulichkhampha.net/wp-content/uploads/2018/12/2.jpg" alt="Du lịch">
-								<h5 class="heading_title">Du lịch</h5>
-							</a>
+				
+						<div class="cate_list" style="background: url(https://dulichkhampha.net/wp-content/uploads/2018/12/3.jpg);
+		background-size: cover;">
+							<a href="https://dulichkhampha.net/am-thuc"></a>
+							<h5 class="heading_title">
+								<a href="https://dulichkhampha.net/am-thuc"><i class="fa fa-map-marker"></i>Ẩm thực</a>
+							</h5>
 						</div>
+						
+						<div class="cate_list" style="background: url(https://dulichkhampha.net/wp-content/uploads/2018/12/4.jpg);
+		background-size: cover;">
+							<a href="https://dulichkhampha.net/lam-dep"></a>
+							<h5 class="heading_title">
+								<a href="https://dulichkhampha.net/lam-dep"><i class="fa fa-map-marker"></i>Làm đẹp</a>
+							</h5>
+						</div>
+						
+						<div class="cate_list" style="background: url(https://dulichkhampha.net/wp-content/uploads/2018/12/5.jpg);
+		background-size: cover;">
+							<a href="https://dulichkhampha.net/suc-khoe"></a>
+							<h5 class="heading_title">
+								<a href="https://dulichkhampha.net/suc-khoe"><i class="fa fa-map-marker"></i>Sức khỏe</a>
+							</h5>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
