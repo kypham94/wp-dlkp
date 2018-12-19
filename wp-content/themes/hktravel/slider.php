40ccdb2c5a4411baa153bbde2dc38b34375cfbf5
kypham94 <41797409+kypham94@users.noreply.github.com>
2018-12-19 19:04:57 +0700

    Update slider.php

--- wp-content/themes/hktravel/slider.php
+++ wp-content/themes/hktravel/slider.php
@@ -18,7 +18,7 @@
 					<div class="item">
 						<div class="row">
 							<div class="col-xs-12 col-sm-12 col-md-8">
-								<?php echo get_the_post_thumbnail( get_the_id(), 'slider-thumb', array('alt' => get_the_title(), 'class' => 'images-slider')); ?>
+								<a href="<?php the_permalink();?>"><?php echo get_the_post_thumbnail( get_the_id(), 'slider-thumb', array('alt' => get_the_title(), 'class' => 'images-slider')); ?></a>
 							</div>
 							<div class="col-xs-12 col-sm-12 col-md-4">
 								<div class="info-slider">
