fda1b540d80c4a796e12a1c0ad4c478eefe9e1cd
kypham94 <41797409+kypham94@users.noreply.github.com>
2018-12-22 10:00:37 +0700

    Update index.php

--- wp-content/themes/hktravel/index.php
+++ wp-content/themes/hktravel/index.php
@@ -1,103 +1,81 @@
 <?php get_header(); ?>
 	<div id="content">
 		<?php get_template_part('slider'); ?>
-		<div class="content-featured">
-			<div class="container">
-				<h2><span>Danh mục nổi bật</span></h2>
-				<div class="main-featured">
-					<div class="box-left">
-						<div class="cate_list" style="background: url(https://dulichkhampha.net/wp-content/uploads/2018/12/gioi-thieu.jpg);
-		background-size: cover;">
-							<a href="https://dulichkhampha.net/gioi-thieu" title="Giới thiệu"></a>
-							<h5 class="heading_title">
-								<a href="https://dulichkhampha.net/gioi-thieu" title="Giới thiệu"><i class="fa fa-map-marker"></i>Giới thiệu</a>
-							</h5>
+			<div class="content-featured">
+				<div class="container">
+					<h2><span>Danh mục nổi bật</span></h2>
+					<div class="main-featured">
+						<div class="box-left">
+							<div class="cate_list" style="background: url(https://dulichkhampha.net/wp-content/uploads/2018/12/gioi-thieu.jpg);
+			background-size: cover;">
+								<a href="https://dulichkhampha.net/gioi-thieu" title="Giới thiệu"></a>
+								<h5 class="heading_title">
+									<a href="https://dulichkhampha.net/gioi-thieu" title="Giới thiệu"><i class="fa fa-map-marker"></i>Giới thiệu</a>
+								</h5>
+							</div>
 						</div>
-					</div>
+						
+						<div class="box-right">
+							<div class="cate_list" style="background: url(https://dulichkhampha.net/wp-content/uploads/2018/12/am-thuc.jpg);
+			background-size: cover;float:right">
+								<a href="https://dulichkhampha.net/am-thuc" title="Ẩm thực"></a>
+								<h5 class="heading_title">
+									<a href="https://dulichkhampha.net/am-thuc" title="Ẩm thực"><i class="fa fa-map-marker"></i>Ẩm thực</a>
+								</h5>
+							</div>
+							
+							<div class="cate_list" style="background: url(https://dulichkhampha.net/wp-content/uploads/2018/12/du-lich.jpg);
+			background-size: cover;">
+								<a href="https://dulichkhampha.net/du-lich" title="Du lịch"></a>
+								<h5 class="heading_title">
+									<a href="https://dulichkhampha.net/du-lich" title="Du lịch"><i class="fa fa-map-marker"></i>Du lịch</a>
+								</h5>
+							</div>
 					
-					<div class="box-right">
-						<div class="cate_list" style="background: url(https://dulichkhampha.net/wp-content/uploads/2018/12/am-thuc.jpg);
-		background-size: cover;float:right">
-							<a href="https://dulichkhampha.net/am-thuc" title="Ẩm thực"></a>
-							<h5 class="heading_title">
-								<a href="https://dulichkhampha.net/am-thuc" title="Ẩm thực"><i class="fa fa-map-marker"></i>Ẩm thực</a>
-							</h5>
-						</div>
-						
-						<div class="cate_list" style="background: url(https://dulichkhampha.net/wp-content/uploads/2018/12/du-lich.jpg);
-		background-size: cover;">
-							<a href="https://dulichkhampha.net/du-lich" title="Du lịch"></a>
-							<h5 class="heading_title">
-								<a href="https://dulichkhampha.net/du-lich" title="Du lịch"><i class="fa fa-map-marker"></i>Du lịch</a>
-							</h5>
-						</div>
-				
-						<div class="cate_list" style="background: url(https://dulichkhampha.net/wp-content/uploads/2018/12/suc-khoe.jpg);
-		background-size: cover;float:right">
-							<a href="https://dulichkhampha.net/suc-khoe" title="Sức khỏe"></a>
-							<h5 class="heading_title">
-								<a href="https://dulichkhampha.net/suc-khoe" title="Sức khỏe"><i class="fa fa-map-marker"></i>Sức khỏe</a>
-							</h5>
-						</div>
-						
-						<div class="cate_list" style="background: url(https://dulichkhampha.net/wp-content/uploads/2018/12/lam-dep.jpg);
-		background-size: cover;">
-							<a href="https://dulichkhampha.net/lam-dep" title="Làm đẹp"></a>
-							<h5 class="heading_title">
-								<a href="https://dulichkhampha.net/lam-dep" title="Làm đẹp"><i class="fa fa-map-marker"></i>Làm đẹp</a>
-							</h5>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div class="content-post">
-			<div class="container">
-				<div role="tabpanel">
-					<ul class="nav nav-tabs post-tabs" role="tablist">
-						<li role="presentation">
-							<a href="#tab1" aria-controls="tab1" role="tab" data-toggle="tab">Bài viết xem nhiều</a>
-						</li>
-					</ul>
-					<div class="tab-content">
-						<div role="tabpanel" class="tab-pane active" id="tab1">
-							<div class="detail-post-main">
-								<ul>
-									<?php $getposts = new WP_query(); $getposts->query('post_status=publish&showposts=5&post_type=post&meta_key=views&orderby=meta_value_num'); ?>
-									<?php global $wp_query; $wp_query->in_the_loop = true; ?>
-									<?php while ($getposts->have_posts()) : $getposts->the_post(); ?>
-										<li>
-											<div class="time-line">
-												<span class="round"></span>
-												<span class="time"><?php echo get_the_date('d/m/Y'); ?></span>
-											</div>
-											<div class="info-post">
-												<div class="row">
-													<div class="col-xs-12 col-sm-12 col-md-8">
-														<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-														<div class="meta"><span>Chia sẻ bởi: <strong><?php the_author(); ?></strong></span></div>
-														<p><?php echo teaser(80); ?></p>
-														<div class="view"><span><?php echo getpostviews(get_the_id()); ?> Lượt xem</span></div>
-													</div>
-													<div class="col-xs-12 col-sm-12 col-md-4">
-														<div class="info-post-img">
-															<a href="<?php the_permalink(); ?>">
-																<?php echo get_the_post_thumbnail( get_the_id(), 'post-thumb', array("alt"=>get_the_title(), 'class' => 'img-post') ); ?>
-															</a>
-														</div>
-													</div>
-												</div>
-											</div>
-											<div class="clear"></div>
-										</li>
-									<?php endwhile; wp_reset_postdata(); ?>
-								</ul>
+							<div class="cate_list" style="background: url(https://dulichkhampha.net/wp-content/uploads/2018/12/suc-khoe.jpg);
+			background-size: cover;float:right">
+								<a href="https://dulichkhampha.net/suc-khoe" title="Sức khỏe"></a>
+								<h5 class="heading_title">
+									<a href="https://dulichkhampha.net/suc-khoe" title="Sức khỏe"><i class="fa fa-map-marker"></i>Sức khỏe</a>
+								</h5>
+							</div>
+							
+							<div class="cate_list" style="background: url(https://dulichkhampha.net/wp-content/uploads/2018/12/lam-dep.jpg);
+			background-size: cover;">
+								<a href="https://dulichkhampha.net/lam-dep" title="Làm đẹp"></a>
+								<h5 class="heading_title">
+									<a href="https://dulichkhampha.net/lam-dep" title="Làm đẹp"><i class="fa fa-map-marker"></i>Làm đẹp</a>
+								</h5>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+			<div class="content-post">
+				<h2><span>Bài viết xem nhiều</span></h2>
+				<div class="container">
+					<?php $getposts = new WP_query(); $getposts->query('post_status=publish&showposts=6&post_type=post&meta_key=views&orderby=meta_value_num'); ?>
+					<?php global $wp_query; $wp_query->in_the_loop = true; ?>
+					<?php while ($getposts->have_posts()) : $getposts->the_post(); ?>
+					<div class="post-inner">
+						<div class="entry-thumb">
+							<a href="<?php the_permalink(); ?>">
+								<?php echo get_the_post_thumbnail( get_the_id(), 'post-thumb', array("alt"=>get_the_title(), 'class' => 'img-post') ); ?>
+							</a>
+						</div>
+						<div class="entry-content">
+							<h4><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></h4>
+						</div>
+						<div class="meta">
+							<span>Ngày đăng: <strong><?php echo get_the_date('d/m/Y'); ?></strong></span>
+							<span>Chia sẻ bởi: <strong><?php the_author(); ?></strong></span>
+						</div>				
+						<div class="meta"><?php echo teaser(80); ?></div>
+						<div class="clear"></div>
+					</div>
+					<?php endwhile; wp_reset_postdata(); ?>
+				</div>
+			</div>
 			<div class="title-head">
 				<div class="container">
 					<div role="tabpanel">
