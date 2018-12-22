b8796eaa89d7ecb789ff02911e556ad67668166f
kypham94 <41797409+kypham94@users.noreply.github.com>
2018-12-22 10:19:16 +0700

    Update index.php

--- wp-content/themes/hktravel/index.php
+++ wp-content/themes/hktravel/index.php
@@ -59,20 +59,22 @@
 					<?php global $wp_query; $wp_query->in_the_loop = true; ?>
 					<?php while ($getposts->have_posts()) : $getposts->the_post(); ?>
 					<div class="post-inner">
-						<div class="entry-thumb">
-							<a href="<?php the_permalink(); ?>">
-								<?php echo get_the_post_thumbnail( get_the_id(), 'post-thumb', array("alt"=>get_the_title(), 'class' => 'img-post') ); ?>
-							</a>
+						<div class="box-post">
+							<div class="entry-thumb">
+								<a href="<?php the_permalink(); ?>">
+									<?php echo get_the_post_thumbnail( get_the_id(), 'post-thumb', array("alt"=>get_the_title(), 'class' => 'img-post') ); ?>
+								</a>
+							</div>
+							<div class="entry-content">
+								<h4><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></h4>
+							</div>
+							<div class="meta_box">
+								<span>Ngày đăng: <strong><?php echo get_the_date('d/m/Y'); ?></strong></span>
+								<span>Chia sẻ bởi: <strong><?php the_author(); ?></strong></span>
+							</div>				
+							<div class="meta-box"><?php echo teaser(80); ?></div>
+							<div class="clear"></div>
 						</div>
-						<div class="entry-content">
-							<h4><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></h4>
-						</div>
-						<div class="meta_box">
-							<span>Ngày đăng: <strong><?php echo get_the_date('d/m/Y'); ?></strong></span>
-							<span>Chia sẻ bởi: <strong><?php the_author(); ?></strong></span>
-						</div>				
-						<div class="meta_box"><?php echo teaser(80); ?></div>
-						<div class="clear"></div>
 					</div>
 					<?php endwhile; wp_reset_postdata(); ?>
 				</div>
