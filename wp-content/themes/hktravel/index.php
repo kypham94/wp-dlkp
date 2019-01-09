929b47c7c76e4205b47918e90d256f12f3bcacd7
kypham94 <41797409+kypham94@users.noreply.github.com>
2019-01-09 14:45:29 +0700

    Update index.php

--- wp-content/themes/hktravel/index.php
+++ wp-content/themes/hktravel/index.php
@@ -62,36 +62,29 @@
 					<div class="tab-content">
 						<div role="tabpanel" class="tab-pane active" id="tab1">
 							<div class="detail-post-main">
-								<ul>
+								<div class="row">
 									<?php $getposts = new WP_query(); $getposts->query('post_status=publish&showposts=5&post_type=post&meta_key=views&orderby=meta_value_num'); ?>
 									<?php global $wp_query; $wp_query->in_the_loop = true; ?>
 									<?php while ($getposts->have_posts()) : $getposts->the_post(); ?>
-										<li>
-											<div class="time-line">
-												<span class="round"></span>
-												<span class="time"><?php echo get_the_date('d/m/Y'); ?></span>
-											</div>
 											<div class="info-post">
 												<div class="row">
-													<div class="col-xs-12 col-sm-12 col-md-8">
-														<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-														<div class="meta"><span>Chia sẻ bởi: <strong><?php the_author(); ?></strong></span></div>
-														<p><?php echo teaser(80); ?></p>
-														<div class="view"><span><?php echo getpostviews(get_the_id()); ?> Lượt xem</span></div>
-													</div>
 													<div class="col-xs-12 col-sm-12 col-md-4">
 														<div class="info-post-img">
 															<a href="<?php the_permalink(); ?>">
 																<?php echo get_the_post_thumbnail( get_the_id(), 'post-thumb', array("alt"=>get_the_title(), 'class' => 'img-post') ); ?>
 															</a>
 														</div>
+														<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+														<div class="meta"><span>Chia sẻ bởi: <strong><?php the_author(); ?></strong></span></div>
+														<p><?php echo teaser(80); ?></p>
+														<div class="view"><span><?php echo getpostviews(get_the_id()); ?> Lượt xem</span></div>
 													</div>
 												</div>
 											</div>
 											<div class="clear"></div>
-										</li>
+										
 									<?php endwhile; wp_reset_postdata(); ?>
-								</ul>
+								</div>
 							</div>
 						</div>
 					</div>
