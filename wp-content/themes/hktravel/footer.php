b1920890b1936ab6d8ae7c30ba568c05f94e7067
kypham94 <41797409+kypham94@users.noreply.github.com>
2019-01-09 13:44:26 +0700

    Update footer.php

--- wp-content/themes/hktravel/footer.php
+++ wp-content/themes/hktravel/footer.php
@@ -132,5 +132,28 @@
 		  gtag('config', 'UA-130880742-1');
 		</script>
 
+
+<script type='text/javascript'>
+//<![CDATA[
+document.write('<div class="tet-2019"><img class="left-2019" id="left-2020" src="https://dulichkhampha.net/wp-content/uploads/2019/01/tet_left.png"/><img class="right-2019" id="right-2020" src="https://dulichkhampha.net/wp-content/uploads/2019/01/tet_right.png"/></div><style>#left-2020{width: 130px;left:0;z-index:7;position:fixed;;transition:all 0.3s linear;-moz-transition:all 0.3s linear;-webkit-transition:all 0.3s linear}#right-2020{width: 130px;right:0;z-index:7;position:fixed;;transition:all 0.3s linear;-moz-transition:all 0.3s linear;-webkit-transition:all 0.3s linear}.left-2019{top:0px}.right-2019{top:0px}.text-2019-l{top:50px}.text-2019-r{top:50px}@media screen and (max-width:1024px){.tet-2019{display:none}}@media screen and (max-width:1440px){#shareduyblogs{display:none}}</style>')
+//]]>
+</script>
+<script>//<![CDATA[
+      var lastScroll = 0;
+      jQuery(document).ready(function($) {
+        $(window).scroll(function() {
+          var scroll = $(window).scrollTop();
+          if (scroll > lastScroll) {
+            $('#right-2020').removeClass('left-2019').addClass('text-2019-l')
+            $('#left-2020').removeClass('right-2019').addClass('text-2019-r')
+          } else if (scroll < lastScroll) {
+            $('#right-2020').addClass('left-2019').removeClass('text-2019-l')
+$('#left-2020').addClass('right-2019').removeClass('text-2019-r')
+          }
+          lastScroll = scroll;
+        })
+      })
+//]]></script>
+
 	</body>
 </html>
