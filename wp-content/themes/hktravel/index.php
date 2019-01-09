d07b331a5150e87030a2f9ed328d2d1488dd8d41
kypham94 <41797409+kypham94@users.noreply.github.com>
2019-01-09 15:17:14 +0700

    Update index.php

--- wp-content/themes/hktravel/index.php
+++ wp-content/themes/hktravel/index.php
@@ -73,7 +73,7 @@
 												</a>
 											</div>
 											<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-											<div class="meta"><span><strong><i>View: </i></strong><?php echo getpostviews(get_the_id()); ?> Lượt xem</span></div>
+											<div class="meta"><span><strong><i>View: </strong><?php echo getpostviews(get_the_id()); ?> Lượt xem</i></span></div>
 											<p><?php echo teaser(80); ?></p>
 										</div>
 									<?php endwhile; wp_reset_postdata(); ?>
