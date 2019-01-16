5a2f5d5177ac020b6fc03bfbd8dcc3eea4551905
kypham94 <41797409+kypham94@users.noreply.github.com>
2019-01-16 14:53:17 +0700

    Update index.php

--- wp-content/themes/hktravel/index.php
+++ wp-content/themes/hktravel/index.php
@@ -147,7 +147,7 @@
 											<div class="box-mota-cate">
 											<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
 											<div class="meta"><span><strong><i>View: </strong><?php echo getpostviews(get_the_id()); ?> Lượt xem</i></span></div>
-											<p><?php echo teaser(80); ?></p>
+											<p><?php echo teaser(30); ?></p>
 											</div>
 											</div>
 										</div>
@@ -171,7 +171,7 @@
 											<div class="box-mota-cate">
 											<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
 											<div class="meta"><span><strong><i>View: </strong><?php echo getpostviews(get_the_id()); ?> Lượt xem</i></span></div>
-											<p><?php echo teaser(80); ?></p>
+											<p><?php echo teaser(30); ?></p>
 											</div>
 											</div>
 										</div>
@@ -195,7 +195,7 @@
 											<div class="box-mota-cate">
 											<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
 											<div class="meta"><span><strong><i>View: </strong><?php echo getpostviews(get_the_id()); ?> Lượt xem</i></span></div>
-											<p><?php echo teaser(80); ?></p>
+											<p><?php echo teaser(30); ?></p>
 											</div>
 											</div>
 											</div>
