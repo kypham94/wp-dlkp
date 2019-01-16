07a449193183ffafa91a800618af43079e6c3cce
kypham94 <41797409+kypham94@users.noreply.github.com>
2019-01-16 14:45:05 +0700

    Update index.php

--- wp-content/themes/hktravel/index.php
+++ wp-content/themes/hktravel/index.php
@@ -120,7 +120,7 @@
 													<?php echo get_the_post_thumbnail( get_the_id(), 'post-thumb', array("alt"=>get_the_title(), 'class' => 'img-post') ); ?>
 												</a>
 											</div>
-											<div class="box-mota">
+											<div class="box-mota-cate">
 											<h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
 											<div class="meta"><span><strong><i>View: </strong><?php echo getpostviews(get_the_id()); ?> Lượt xem</i></span></div>
 											<p><?php echo teaser(30); ?></p>
